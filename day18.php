d265d859d4ee4582a38414c8a446bd4fe2a3701e
Nate Denlinger <pitchinnate@gmail.com>
2016-12-20 13:46:25 -0500

    day18 part 1

---
+++ day18.php
@@ -0,0 +1,51 @@
+<html>
+<body>
+<pre>
+
+<?php
+const SAFE = '.';
+const TRAP = '^';
+
+$grid = [
+    1 => '.^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....',
+];
+$max_levels = 40;
+$currentLevel = 1;
+
+while($currentLevel < $max_levels) {
+    $lastRow = $grid[$currentLevel];
+    $currentLevel++;
+    $grid[$currentLevel] = createNewRow($lastRow);
+}
+$allTraps = implode('',$grid);
+echo substr_count($allTraps,'.');
+
+function createNewRow($lastRow)
+{
+    $letters = str_split($lastRow);
+    array_unshift($letters,'.');
+    array_push($letters,'.');
+    $letterCount = count($letters) - 1;
+    $newRow = "";
+    for($x=1;$x<$letterCount;$x++) {
+        $newRow .= checkTrap($letters[($x - 1)],$letters[$x],$letters[($x + 1)]);
+    }
+    return $newRow;
+}
+
+function checkTrap($firstLetter,$secondLetter,$thirdLetter)
+{
+    if($firstLetter == TRAP && $secondLetter == TRAP && $thirdLetter == SAFE) {
+        return TRAP;
+    }
+    if($firstLetter == SAFE && $secondLetter == TRAP && $thirdLetter == TRAP) {
+        return TRAP;
+    }
+    if($firstLetter == TRAP && $secondLetter == SAFE && $thirdLetter == SAFE) {
+        return TRAP;
+    }
+    if($firstLetter == SAFE && $secondLetter == SAFE && $thirdLetter == TRAP) {
+        return TRAP;
+    }
+    return SAFE;
+}
