f5833df7f3b52366e7286e4fa8c34ced4af0fa6a
Nate Denlinger <pitchinnate@gmail.com>
2016-12-21 01:19:55 -0500

    day 20 part 2

--- day20.php
+++ day20.php
@@ -1,26 +1,59 @@
 <?php
 
 include('day20_input.php');
-//$input = "5-8
-//0-2
-//4-7";
 
 $maxIp = 4294967295;
-//$maxIp = 10;
-
-$minIp = 0;
 
 $lines = explode("\n",$input);
 $blacklist = [];
 foreach($lines as $line) {
     $pieces = explode('-',$line);
-    $blacklist[$pieces[0]] = $pieces[1];
+    $blacklist[$pieces[0]] = [
+        'min' => $pieces[0],
+        'max' => $pieces[1],
+    ];
 }
 ksort($blacklist);
+$blacklist = array_values($blacklist);
+$removed = 1;
 
-foreach($blacklist as $min => $max) {
-    if($minIp >= $min && $minIp <= $max) {
-        $minIp = $max + 1;
+while($removed > 0) {
+    $removed = 0;
+    $blacklistCount = count($blacklist);
+    $newBlackList = [];
+    for ($x = 0; $x < $blacklistCount; $x++) {
+        if($x == ($blacklistCount - 1)) {
+            $newBlackList[] = $blacklist[$x];
+            break;
+        }
+        $min = (int)$blacklist[$x]['min'];
+        $max = (int)$blacklist[$x]['max'];
+        $y = $x + 1;
+        if ($blacklist[$y]['min'] >= $min && $blacklist[$y]['min'] <= $max) {
+            if ($max < $blacklist[$y]['max']) {
+                $max = $blacklist[$y]['max'];
+            }
+            $removed++;
+            $x++;
+        } else {
+            if ($blacklist[$y]['min'] == ($max + 1)) {
+                $max = $blacklist[$y]['max'];
+                $removed++;
+                $x++;
+            }
+        }
+        $newBlackList[] = [
+            'min' => $min,
+            'max' => $max,
+        ];
     }
+    $blacklist = $newBlackList;
 }
-echo $minIp;
+
+$validIps = 0;
+$blacklistCount = count($blacklist) - 1;
+for($x=0;$x<$blacklistCount;$x++) {
+    $goodRange = $blacklist[($x+1)]['min'] - $blacklist[$x]['max'] - 1;
+    $validIps += $goodRange;
+}
+echo "valid ips: $validIps";
