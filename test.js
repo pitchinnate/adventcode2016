17775eead1eeba91fc057ecd7c90e9b9af7e6edf
Nate Denlinger <pitchinnate@gmail.com>
2016-12-30 09:53:43 -0500

    playing with benchmarking again

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-var numberOfElves = 3012210;
+var numberOfElves = 3012000;
 var elves = [...Array((numberOfElves+1)).keys()];
 
-var last_time = new Date();
+var last_time = process.hrtime()[1];
 elves.splice(2,1);
-var now = new Date();
+var now = process.hrtime()[1];
 var diff = now - last_time;
-last_time = now;
 console.log(" time: " + diff);
 
+last_time = process.hrtime()[1];
 elves.splice(3000000,1);
-now = new Date();
+now = process.hrtime()[1];
 diff = now - last_time;
 last_time = now;
 console.log(" time: " + diff);
